dea1fc4e3ef597b5841be4e950ca676355f11811
ViktorKozh <viktor.ru253@gmail.com>
2023-12-20 20:40:17 +0300

    Forgot pdf

---
+++ indiv 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def create_replacer(replace_char):
+    def replace_duplicates(string):
+        new_string = ""
+        previous_char = ""
+        for char in string:
+            if char != previous_char:
+                new_string += char
+            else:
+                new_string += replace_char
+            previous_char = char
+        return new_string
+
+    return replace_duplicates
+
+
+if __name__ == '__main__':
+    inp = input("Введите слово с повторяющимися буквами: ")
+    repl_dup = create_replacer(
+        input("Введите символ для замены повторяющихся букв: "))
+    print(repl_dup(inp))
